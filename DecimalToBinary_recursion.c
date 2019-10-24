7d8a6d50b0c2c602065b63fbe61d3dfadfc0b6ab
Sai Ankit <saiankit30@gmail.com>
2019-10-24 11:51:19 +0530

    Issue regarding the reversal recursion has been resolved (#10)

--- DecimalToBinary_recursion.c
+++ DecimalToBinary_recursion.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
-int binreversed(int n , int s)
+int bin(int n)
 {
-    if(n==1)
+    if(n==0)
         {
-            return (s*10)+(n%2) ;
+            return 0 ;
         }
     else
     {
-        return binreversed(n/2,(s*10)+(n%2));
-    }
-}
-int rev(int a,int b)
-{
-    if (a <=9)
-        return (b*10)+(a%10);
-    else
-    {
-        return rev(a/10,(b*10)+(a%10));
+        return 10*(bin(n/2))+(n%2);
     }
 }
 int main()
 {
-    int n,x,BinRev;
-    int s=0,y=0;
+    int n,x;
     printf("Please enter the decimal number \n");
     scanf("%d",&x);
     n = x;
-    BinRev = binreversed(n,s);
-    printf("The binary equivalent of %d is %d \n",x,rev(BinRev,y));
+    printf("The binary equivalent of %d is %d \n",x,bin(n));
 }

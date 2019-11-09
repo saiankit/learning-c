2c5f08ddcee78f069fb6ec8522382b020f2d1a9b
saiankit <saiankit30@gmail.com>
2019-11-09 12:45:38 +0530

    Added a code that compares two strings entered

---
+++ StringCompare.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char str1[10000],str2[10000];
+	printf("Enter the number1.\n");
+	scanf("%s",str1);
+	printf("Enter the number2.\n");
+	scanf("%s",str2);
+	if (strcmp(str1,str2)==1||strcmp(str1,str2)==-1)
+	{
+		printf("The strings are not same.\n");
+	}
+	else 
+	{
+		printf("The strings are same.\n");
+	}
+	return 0;
+}

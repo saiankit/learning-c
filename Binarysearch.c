5909372d7c6154b4ec5b30e3326207b23ba588ee
saiankit <saiankit30@gmail.com>
2019-11-15 13:58:32 +0530

    Added a code to perform Binary Search using standard Library function bsearch()

---
+++ Binarysearch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *)a-*(int *)b);
+}
+int main()
+{
+    int arr[100];
+    int n,found,element;
+    found =0;
+    //InputArray
+    printf("Please enter the number of elements.\n");
+    scanf("%d",&n);
+    for (int i = 0; i <n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the element you are looking for. \n");
+    scanf("%d",&element);
+    int *item;
+    item = (int *)bsearch(&element,arr,n,sizeof(int),cmpfunc);
+    if (item!=0)
+    {
+        printf("Element is found.\n");
+    }
+    else
+    {
+        printf("Element is not found. \n");
+    }
+}
